00245e65aa91dfc79826bb0562bf900d34bd1b67
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-08-10 09:02:13 +0300

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use solana_sdk::pubkey::Pubkey;
 
 // USDC mint address on Solana mainnet
 pub const USDC_MINT: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
@@ -73,6 +74,57 @@ pub struct TransactionMeta {
     pub inner_instructions: Option<Vec<InnerInstructions>>,
     #[serde(rename = "logMessages")]
     pub log_messages: Option<Vec<String>>,
+    #[serde(rename = "postBalances")]
+    pub post_balances: Vec<u64>,
+    #[serde(rename = "postTokenBalances")]
+    pub post_token_balances: Option<Vec<TokenBalance>>,
+    #[serde(rename = "preBalances")]
+    pub pre_balances: Vec<u64>,
+    #[serde(rename = "preTokenBalances")]
+    pub pre_token_balances: Option<Vec<TokenBalance>>,
+    pub status: serde_json::Value,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct InnerInstructions {
+    pub index: u8,
+    pub instructions: Vec<InnerInstruction>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct InnerInstruction {
+    pub accounts: Vec<u8>,
+    pub data: String,
+    #[serde(rename = "programId")]
+    pub program_id: String,
+    #[serde(rename = "stackHeight")]
+    pub stack_height: Option<u32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TokenBalance {
+    #[serde(rename = "accountIndex")]
+    pub account_index: u8,
+    pub mint: String,
+    #[serde(rename = "uiTokenAmount")]
+    pub ui_token_amount: UiTokenAmount,
+    pub owner: Option<String>,
+    #[serde(rename = "programId")]
+    pub program_id: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UiTokenAmount {
+    pub amount: String,
+    pub decimals: u8,
+    #[serde(rename = "uiAmount")]
+    pub ui_amount: Option<f64>,
+    #[serde(rename = "uiAmountString")]
+    pub ui_amount_string: String,
+}
+
+// SPL Token program ID
+pub const SPL_TOKEN_PROGRAM_ID: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
     #[serde(rename = "postBalances")]
     pub post_balances: Vec<u64>,
     #[serde(rename = "postTokenBalances")]
